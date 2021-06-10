0837db0381749d3ab9dfd6a5cf904f91b2e7f753
ibsio <ibsiology@gmail.com>
2021-06-11 09:59:50 +1000

    pulled displaying-coordinates component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import { PlayerCard } from './PlayerCard'
-// import PlayerCard2 from './PlayerCard2'
 import {SelectPlayers} from './SelectPlayers'
 
 const App = () => {
   return (
     <div >
           The basic empty template
-          {/* <PlayerCard2 /> */}
           <SelectPlayers />
-          <PlayerCard name="Emily"/>
-          <PlayerCard name="Derick"/>
-          <PlayerCard name="Ihab"/>
     </div>
   )
 }
