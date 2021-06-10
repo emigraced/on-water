7dea0d407ff0ac372945ebb705439f9b49ba7cf3
emigraced <helloemilymills@gmail.com>
2021-06-10 11:22:39 +1000

    removed player card component from app.js since it will be called by the select players component. adjusted name prop to id.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { PlayerCard } from './PlayerCard'
 
 const App = () => {
   return (
     <div >
-          The basic empty template
-
-          <PlayerCard name="Emily"/>
-          <PlayerCard name="Derick"/>
-          <PlayerCard name="Ihab"/>
+          
     </div>
   )
 }
